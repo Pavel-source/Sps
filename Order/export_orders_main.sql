e56bead8e0ce8fcfa62c90e0f4c03c5e19feb6b9
Pavel-source <71154059+Pavel-source@users.noreply.github.com>
2022-08-18 12:09:48 +0200

    orders

--- Order/export_orders_main.sql
+++ Order/export_orders_main.sql
@@ -115,55 +115,108 @@ gift_product_variants AS
 		and pge.productstandardgift IN (1142811913, 1142811979, 1142811934, 1142813653, 1142811940) 
 ),
 
+cte_mobileByContact as (
+    select contactid,
+           max(number_) mobile_phone,
+    from phonenumber pn
+	where type = 'MOBILE_PHONE'
+    group by contactid
+),
+
+cte_mobileByCustomer as (
+    select customerid,
+           max(number_) mobile_phone,
+    from phonenumber pn
+	where type = 'MOBILE_PHONE'
+    group by customerid
+),
+
 cte_Individualshipping AS
 (
 SELECT
-   o.id,
-   0 AS version,
+   o.ORDERCODE AS id,
    o.Created AS createdAt,
-   o.ordercode AS orderReference, 
+   '0' AS orderReference, 
    o.customerid AS customerId,
    cr.email AS customerEmail,
    o.grandtotalforpayment AS totalPrice,
    (sbp.priceWithVat + sbp.discountWithVat) AS totalShippingPrice,
    
 	CONCAT('{',
-		'"id": ', CONCAT('"delivery_', o.id, '"'),
+		'"id": ', CONCAT('"delivery_', o.ORDERCODE, '"'),
 		IFNULL(CONCAT(',"status": ', CONCAT('"', sds.currentState, '"')), ''), 
 		IFNULL(CONCAT(',"firstName": ', CONCAT('"', r.firstname, '"')), ''), 
 		IFNULL(CONCAT(',"lastName": ', CONCAT('"', r.lastname, '"')), ''), 
 		IFNULL(CONCAT(',"deliveryType": ', CONCAT('"', dp.type, '"')), ''), 
 		
 		',"address": ', 	IFNULL(CONCAT('{',
-												TRIM(LEADING ',' FROM CONCAT(
-												IFNULL(CONCAT('"zipPostalCode": ', CONCAT('"', IFNULL(a.zippostalcode, a2.zippostalcode), '"')), ''),   
-												IFNULL(CONCAT(',"countryCode": ', CONCAT('"', IFNULL(a.countrycode, a2.countrycode), '"')), ''),   
-												IFNULL(CONCAT(',"state": ', CONCAT('"', IFNULL(a.STATEPROVINCECOUNTY, a2.STATEPROVINCECOUNTY), '"')), ''), 
-												IFNULL(CONCAT(',"street": ', CONCAT('"', IFNULL(a.street, a2.street), '"')), ''), 
-												IFNULL(CONCAT(',"city": ', CONCAT('"', IFNULL(a.city, a2.city), '"')), ''), 
-												IFNULL(CONCAT(',"streetNumber": ', CONCAT('"', IFNULL(a.streetnumber, a2.streetnumber), '"')), ''), 
+												CONCAT('"id": ', '"', CONCAT('fake-', UUID()), '"'),
 												IFNULL(CONCAT(',"firstName": ', CONCAT('"', r.firstname, '"')), ''), 
-												IFNULL(CONCAT(',"lastName": ', CONCAT('"', r.lastname, '"')), ''), 
-												IFNULL(CONCAT(',"phone": ', CONCAT('"', r.phonenumber, '"')), ''), 
-												IFNULL(CONCAT(',"email": ', CONCAT('"', cea.email, '"')), '')
-												)), 
+												IFNULL(CONCAT(',"lastName": ', CONCAT('"', r.lastname, '"')), ''), 	
+												',"title": null', 	
+												',"addressFirstLine": null', 	
+												IFNULL(CONCAT(',"houseNumber": ', CONCAT('"', case when a.ID IS NOT NULL then a.streetnumber else a2.streetnumber end, '"')), ''), 
+												IFNULL(CONCAT(',"houseNumberExtension": ', CONCAT('"', case when a.ID IS NOT NULL then a.streetnumberextension else a2.streetnumberextension end, '"')), ''), 
+												IFNULL(CONCAT(',"extraAddressLine": ', case when a.ID IS NOT NULL then 
+																								case when COALESCE(a.extraaddressline1, a.extraaddressline2, a.extraaddressline3) IS NULL
+																									then NULL
+																								else
+																									trim(concat(nvl(a.extraaddressline1, ''), ' ', nvl(a.extraaddressline2, ''), ' ', nvl(a.extraaddressline3, '')))
+																								end
+																						else 
+																								case when COALESCE(a2.extraaddressline1, a2.extraaddressline2, a2.extraaddressline3) IS NULL
+																									then NULL
+																								else
+																									trim(concat(nvl(a2.extraaddressline1, ''), ' ', nvl(a2.extraaddressline2, ''), ' ', nvl(a2.extraaddressline3, '')))
+																								end
+																						end
+															 )
+													, ''),												
+												
+												IFNULL(CONCAT(',"streetName": ', CONCAT('"', case when a.ID IS NOT NULL then a.street else a2.street end, '"')), ''), 
+												IFNULL(CONCAT(',"city": ', CONCAT('"', case when a.ID IS NOT NULL then a.city else a2.city end, '"')), ''), 
+												IFNULL(CONCAT(',"state": ', CONCAT('"', case when a.ID IS NOT NULL then a.STATEPROVINCECOUNTY else a2.STATEPROVINCECOUNTY end, '"')), ''), 												
+												IFNULL(CONCAT(',"postcode": ', CONCAT('"', case when a.ID IS NOT NULL then a.zippostalcode else a2.zippostalcode end, '"')), ''),   
+												IFNULL(CONCAT(',"country": ', CONCAT('"', case when a.ID IS NOT NULL then c.ENGLISHCOUNTRYNAME else c2.ENGLISHCOUNTRYNAME end, '"')), ''),   
+												IFNULL(CONCAT(',"emailAddress": ', CONCAT('"', cea.email, '"')), ''),
+												IFNULL(CONCAT(',"isMyAddress": ', case when a.ID IS NOT NULL then 'false' when a2.ID IS NOT NULL then 'true' end), ''),
 												'}'), 'null'),
 			
+
+-- "recipientAddress" is the same as "address"
 		',"recipientAddress": ', 	IFNULL(CONCAT('{',
-												TRIM(LEADING ',' FROM CONCAT(
-												IFNULL(CONCAT('"zipPostalCode": ', CONCAT('"', IFNULL(a.zippostalcode, a2.zippostalcode), '"')), ''),   
-												IFNULL(CONCAT(',"countryCode": ', CONCAT('"', IFNULL(a.countrycode, a2.countrycode), '"')), ''),   
-												IFNULL(CONCAT(',"state": ', CONCAT('"', IFNULL(a.STATEPROVINCECOUNTY, a2.STATEPROVINCECOUNTY), '"')), ''), 
-												IFNULL(CONCAT(',"street": ', CONCAT('"', IFNULL(a.street, a2.street), '"')), ''), 
-												IFNULL(CONCAT(',"city": ', CONCAT('"', IFNULL(a.city, a2.city), '"')), ''), 
-												IFNULL(CONCAT(',"streetNumber": ', CONCAT('"', IFNULL(a.streetnumber, a2.streetnumber), '"')), ''), 
+												CONCAT('"id": ', '"', CONCAT('fake-', UUID()), '"'),
 												IFNULL(CONCAT(',"firstName": ', CONCAT('"', r.firstname, '"')), ''), 
-												IFNULL(CONCAT(',"lastName": ', CONCAT('"', r.lastname, '"')), ''), 
-												IFNULL(CONCAT(',"phone": ', CONCAT('"', r.phonenumber, '"')), ''), 
-												IFNULL(CONCAT(',"email": ', CONCAT('"', cea.email, '"')), '')
-												)), 
+												IFNULL(CONCAT(',"lastName": ', CONCAT('"', r.lastname, '"')), ''), 	
+											--	',"title": null', 	
+											--	',"addressFirstLine": null', 	
+												IFNULL(CONCAT(',"houseNumber": ', CONCAT('"', case when a.ID IS NOT NULL then a.streetnumber else a2.streetnumber end, '"')), ''), 
+												IFNULL(CONCAT(',"houseNumberExtension": ', CONCAT('"', case when a.ID IS NOT NULL then a.streetnumberextension else a2.streetnumberextension end, '"')), ''), 
+												IFNULL(CONCAT(',"extraAddressLine": ', case when a.ID IS NOT NULL then 
+																								case when COALESCE(a.extraaddressline1, a.extraaddressline2, a.extraaddressline3) IS NULL
+																									then NULL
+																								else
+																									trim(concat(nvl(a.extraaddressline1, ''), ' ', nvl(a.extraaddressline2, ''), ' ', nvl(a.extraaddressline3, '')))
+																								end
+																						else 
+																								case when COALESCE(a2.extraaddressline1, a2.extraaddressline2, a2.extraaddressline3) IS NULL
+																									then NULL
+																								else
+																									trim(concat(nvl(a2.extraaddressline1, ''), ' ', nvl(a2.extraaddressline2, ''), ' ', nvl(a2.extraaddressline3, '')))
+																								end
+																						end
+															 )
+													, ''),												
+												
+												IFNULL(CONCAT(',"streetName": ', CONCAT('"', case when a.ID IS NOT NULL then a.street else a2.street end, '"')), ''), 
+												IFNULL(CONCAT(',"city": ', CONCAT('"', case when a.ID IS NOT NULL then a.city else a2.city end, '"')), ''), 
+												IFNULL(CONCAT(',"state": ', CONCAT('"', case when a.ID IS NOT NULL then a.STATEPROVINCECOUNTY else a2.STATEPROVINCECOUNTY end, '"')), ''), 												
+												IFNULL(CONCAT(',"postcode": ', CONCAT('"', case when a.ID IS NOT NULL then a.zippostalcode else a2.zippostalcode end, '"')), ''),   
+												IFNULL(CONCAT(',"country": ', CONCAT('"', case when a.ID IS NOT NULL then c.ENGLISHCOUNTRYNAME else c2.ENGLISHCOUNTRYNAME end, '"')), ''),   
+												IFNULL(CONCAT(',"emailAddress": ', CONCAT('"', cea.email, '"')), ''),
+												IFNULL(CONCAT(',"isMyAddress": ', case when a.ID IS NOT NULL then 'false' when a2.ID IS NOT NULL then 'true' end), ''),
 												'}'), 'null'),
-			
+
 		IFNULL(CONCAT(',"deliveryDate": ', CONCAT('"', cast(cast(sds.deliveredTime AS DATE) AS VARCHAR(50)), '"')), ''), 
 		-- estimatedDispatchDate
 		IFNULL(CONCAT(',"actualDispatchDate": ', CONCAT('"', cast(cast(dp.pickupDate AS DATE) AS VARCHAR(50)), '"')), ''),  
@@ -178,7 +231,9 @@ SELECT
 		',"orderItems": ',	concat('[',
 						group_concat(
 							CONCAT('{',
-							   '"lineItemId": ', ol.ORDERLINEIDX,
+							   '"id": ', ol.id,
+							   '"previewImages": null', 
+						--	   '"lineItemId": ', ol.ORDERLINEIDX,
 							    IFNULL(CONCAT(',"title": ', CONCAT('"', gpv.nl_product_name, '"')), ''),  
 							--    IFNULL(CONCAT(',"titleEn": ', CONCAT('"', gpv.en_product_name, '"')), ''), 
 							   ',"quantity": ', ol.productamount,
@@ -187,11 +242,13 @@ SELECT
 							--   ',"taxCategory": ',  CONCAT('"', concat('vat', ol.vatcode), '"'),
 							   ',"productType": ', CONCAT('"', gpv.productTypeKey, '"'),
 							   ',"skuId": ', CONCAT('"', gpv.sku_id, '"'), 
-							   ',"productKey": ', CONCAT('"', gpv.productKey, '"'),
 							   ',"productSlug": ', CONCAT('"', gpv.productKey, '"'),
+							   ',"productKey": ', CONCAT('"', gpv.productKey, '"'),
 										'}')
 									)
 							, ']'),
+							
+		IFNULL(CONCAT(',"mobileNumber": ', CONCAT('"', case when a.ID IS NOT NULL then cte_mobileByContact.mobile_phone else cte_mobileByCustomer.mobile_phone end, '"')), ''), 
 		'}'				
 		)
 	AS orderDelivery
@@ -219,6 +276,8 @@ FROM
        on isp.recipientid = r.id
    left join address a
        on r.addressid = a.id
+   left join country c
+       on a.countrycode = c.TWOLETTERCOUNTRYCODE
    left join contactemailaddress cea
 	   on r.contactid = cea.contactid
 		  and cea.emailidx = 0
@@ -228,12 +287,18 @@ FROM
        on psi.type = ct.type and psi.carrier = ct.carrier
    left join address a2
       on cr.id = a2.customerid and a2.DEFAULTADDRESS = 'Y'
+   left join country c2
+       on a2.countrycode = c2.TWOLETTERCOUNTRYCODE
    left join channel ch
 	  on o.channelid = ch.id
    left join shoppingbasketprice sbp
 	  on isp.INDIVIDUALSHIPPINGPRICEID = sbp.id
    left join vat v
 		on sbp.vatId = v.id
+   left join cte_mobileByContact mct
+		on mct.contactid = r.contactid
+   left join cte_mobileByCustomer mcm
+		on mcm.customerid = cr.id
 
 WHERE
    (
@@ -271,7 +336,7 @@ AS
 (
 SELECT
    i.id,
-   i.version,
+   0 AS version,
    i.createdAt,
    i.orderReference, 
    i.customerId,
@@ -284,17 +349,17 @@ SELECT
    p.totalPriceGross,
    p.totalDiscount,
    p.totalItems,
-   0 as creditsUsed,
+   0 AS creditsUsed,
    
 	concat('[',
 	TRIM(LEADING ',' FROM CONCAT(
-		group_concat(
-			   IFNULL(CONCAT(',', i.orderDelivery), '')
-					)
+		group_concat(IFNULL(i.orderDelivery, ''))
 	))
 			, ']')
 	
-	AS orderDeliveries
+	AS orderDeliveries,
+	
+	'S3' AS dataSource
 	
 FROM cte_Individualshipping i
 	 LEFT JOIN cte_Prices p ON i.id = p.id
@@ -309,25 +374,26 @@ SELECT
 				
 				group_concat(
 							CONCAT('{',
-										 '"id": ', id,
+										 '"id": "', id, '"',
 										 ',"version": ', version,
 										 ',"createdAt": ', '"', createdAt, '"',
 										 ',"orderReference": ', '"', orderReference, '"',
 										 ',"customerId": ', customerId,
 										  IFNULL(CONCAT(',"customerEmail": ', CONCAT('"', customerEmail, '"')), ''),
 										  ',"store": ', '"', store, '"',
-										  ',"totalPrice": ', totalPrice,
-										  ',"totalItemPrice": ', totalItemPrice,
-										  ',"totalShippingPrice": ', totalShippingPrice,
-										  ',"totalTaxExclusive": ', totalTaxExclusive,
-										  ',"totalPriceGross": ', totalPriceGross,
-										  ',"totalDiscount": ', totalDiscount,
-										  ',"totalItems": ', totalItems,
+										  IFNULL(CONCAT(',"totalPrice": ', totalPrice), ''),
+										  IFNULL(CONCAT(',"totalItemPrice": ', totalItemPrice), ''),
+										  IFNULL(CONCAT(',"totalShippingPrice": ', totalShippingPrice), ''),
+										  IFNULL(CONCAT(',"totalTaxExclusive": ', totalTaxExclusive), ''),
+										  IFNULL(CONCAT(',"totalPriceGross": ', totalPriceGross), ''),
+										  IFNULL(CONCAT(',"totalDiscount": ', totalDiscount), ''),
+										  IFNULL(CONCAT(',"totalItems": ', totalItems), ''),
 										  ',"creditsUsed": ', creditsUsed,
 										  
 										--  IFNULL(CONCAT(',"orderState": ', CONCAT('"', orderState, '"')), ''),
 										--  IFNULL(CONCAT(',"paymentState": ', CONCAT('"', paymentState, '"')), ''),										 										 
 										 ',"orderDeliveries": ', orderDeliveries,
+										  ',"dataSource": ', '"', dataSource, '"',
 										 '}'
 										 
 										))
